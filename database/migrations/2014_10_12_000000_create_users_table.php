abe7587ea984ddba9c8515af3d6f747a61ea6f58
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2024-01-31 16:31:52 +0100

    #3 migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->dateTime('birthdate');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number');
+            $table->integer('role');
             $table->rememberToken();
             $table->timestamps();
         });
