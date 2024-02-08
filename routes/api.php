c494b1f2935106a5c0cc624cd5542dc32a6c8107
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2024-02-08 09:17:23 +0100

    #16 Route with middleware

--- routes/api.php
+++ routes/api.php
@@ -18,36 +18,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/users', [UserController::class,'index']);
+    Route::get('/user/{id}', [UserController::class,'show']);
+    Route::patch('/product/{id}',[ProductController::class,'update']);
+    Route::delete('/product/{id}',[ProductController::class,'destroy']);
+    Route::post('/category',[CategoryController::class,'store']);
+    Route::patch('/category/{id}',[CategoryController::class,'update']);
+    Route::delete('/category/{id}',[CategoryController::class,'destroy']);
+    Route::get('orders',[OrderController::class,'index']);
+    Route::get('/order/{id}', [OrderController::class,'show']);
+    Route::post('/order',[OrderController::class,'store']);
+    Route::patch('/order/{id}',[OrderController::class,'update']);
+    Route::delete('/order/{id}',[OrderController::class,'destroy']);
 });
+
+
 //route define by apiResource
 //Route::apiResource('products', ProductController::class);
-Route::get('/users', [UserController::class,'index']);
 
-Route::get('/user/{id}', [UserController::class,'show']);
 
 
 Route::get('/products', [ProductController::class,'index']);
 Route::get('/product/{id}', [ProductController::class,'show']);
+
+
 Route::get('/products/category/{id}',[ProductController::class,'productsByCategory']);
 Route::post('/product',[ProductController::class,'store']);
-Route::patch('/product/{id}',[ProductController::class,'update']);
-Route::delete('/product/{id}',[ProductController::class,'destroy']);
+
 
 
 Route::get('categories',[CategoryController::class,'index']);
 Route::get('/category/{id}', [CategoryController::class,'show']);
-Route::post('/category',[CategoryController::class,'store']);
-Route::patch('/category/{id}',[CategoryController::class,'update']);
-Route::delete('/category/{id}',[CategoryController::class,'destroy']);
 
 
-Route::get('orders',[OrderController::class,'index']);
-Route::get('/order/{id}', [OrderController::class,'show']);
-Route::post('/order',[OrderController::class,'store']);
-Route::patch('/order/{id}',[OrderController::class,'update']);
-Route::delete('/order/{id}',[OrderController::class,'destroy']);
+
+
 // Route::get('/post', [PostController::class,'show']);
 // Route::get('/post/{id}', [PostController::class,'showId']);
 // Route::get('/posts', [PostController::class,'index']);
