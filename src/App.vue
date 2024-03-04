87946996eef7a7ff60235cb49e97930bb6e9da68
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2024-03-04 11:56:19 +0100

    commit for postcss

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,16 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Button
+  </button>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <div>
-    <PhHorse />
+    <PhHorse/>
     <PhHeart  color="hotpink" weight="fill" />
-    <PhCube />
+    <PhCube/>
     <PhAlien/>
   </div>
   <header>
@@ -27,65 +33,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
