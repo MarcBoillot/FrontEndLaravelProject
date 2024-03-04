891d102056664bba2467c2cea3190e5976e07ca5
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2024-03-04 10:55:00 +0100

    commit for auto import with unplugin use phosphor

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <div>
+    <PhHorse />
+    <PhHeart  color="hotpink" weight="fill" />
+    <PhCube />
+    <PhAlien/>
+  </div>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
